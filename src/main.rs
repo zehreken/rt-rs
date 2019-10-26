601d67753360eedd48ee3e7680b5e1e66cc36579
zehreken <zehreken@gmail.com>
2019-10-26 15:18:48 +0200

    Add image creation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+extern crate image;
 mod primitives;
 pub use crate::primitives::vec3::*;
+
+pub const WIDTH: u32 = 800;
+pub const HEIGHT: u32 = 400;
 
 fn main() {
     let _vector = Vec3::zero();
     println!("{}", _vector);
+
+    let mut img_buf = image::ImageBuffer::new(WIDTH, HEIGHT);
+    for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
+    }
+    img_buf.save("out/basic.png").unwrap();
 }
