87fdf5b73d749128a8d3e77293aa3546dde72ced
zehreken <zehreken@gmail.com>
2019-10-26 21:03:32 +0200

    Render first circle

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,39 @@ use crate::sphere::sphere::*;
 
 pub const WIDTH: u32 = 800;
 pub const HEIGHT: u32 = 400;
+pub const SAMPLE: u32 = 100;
 
 fn main() {
     let _vector = Vec3::zero();
     println!("{}", _vector);
 
     let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal= Vec3::new(4.0, 0.0, 0.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
 
     let mut img_buf = image::ImageBuffer::new(WIDTH, HEIGHT);
     for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
         let u: f32 = x as f32 / WIDTH as f32;
-        // invert y
-        let v: f32 = (HEIGHT - y) as f32 / HEIGHT as f32;
+        let v: f32 = (HEIGHT - y) as f32 / HEIGHT as f32; // invert y
 
         let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
         let color = color(ray);
-        *pixel = image::Rgb([(color.r() * 255.0) as u8, (color.g() * 255.0) as u8, (color.b() * 255.0) as u8]);
+        *pixel = image::Rgb([
+            (color.r() * 255.0) as u8,
+            (color.g() * 255.0) as u8,
+            (color.b() * 255.0) as u8,
+        ]);
     }
     img_buf.save("out/basic.png").unwrap();
 }
 
 fn color(ray: Ray) -> Vec3 {
+    let sphere: Sphere = Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5);
+    if sphere.hit(ray, 1.0, 100.0) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction: Vec3 = ray.direction().unit_vector();
     let t: f32 = 0.5 * (unit_direction.y() + 1.0);
 
--- src/sphere.rs
+++ src/sphere.rs
@@ -23,8 +23,19 @@ pub mod sphere {
             }
         }
 
-        pub fn hit(ray: Ray, t_min: f32, t_max: f32, hit_record: &HitRecord) -> bool {
-            return false;
+        pub fn hit(
+            self,
+            ray: Ray,
+            t_min: f32,
+            t_max: f32, /*, hit_record: &HitRecord*/
+        ) -> bool {
+            let origin_to_center: Vec3 = ray.origin() - self.center;
+            let a: f32 = Vec3::dot(ray.direction(), ray.direction());
+            let b: f32 = 2.0 * Vec3::dot(origin_to_center, ray.direction());
+            let c: f32 = Vec3::dot(origin_to_center, origin_to_center) - self.radius * self.radius;
+            let discriminant: f32 = b * b - 4.0 * a * c;
+
+            return discriminant > 0.0;
         }
     }
 }
